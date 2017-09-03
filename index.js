e81ba70df90abf4b088f444243b60cf983ad6f0f
Adam Walter <adam.iverson.walter@gmail.com>
2017-09-03 17:18:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelectorAll(selector)[0]
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+
+  for (var i = 0; i < lis.length; i++) {
+  parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+
+}
